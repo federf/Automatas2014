2f1ceee7e3cfafe62a7893150eb8250e23c614ae
federf <fede_franco_93@hotmail.com>
2014-05-14 10:31:36 -0300

    modificacion main, correccion path archivos .dot

--- src/Main.java
+++ src/Main.java
@@ -10,13 +10,13 @@ public class Main {
     public static void main(String[] args) {
         try {
             System.out.println("prueba funcionalidad test1-IntegrationTests.java");
-            DFA my_dfa = (DFA) FA.parse_form_file("test/test/dfa1.dot");
+            DFA my_dfa = (DFA) FA.parse_form_file("test/dfa1.dot");
             System.out.println("acepta ab?:" + my_dfa.toNFA().accepts("ab"));
             System.out.println("acepta abbbbb?:" + my_dfa.toNFA().accepts("abbbbb"));
             System.out.println("acepta bbbbb?:" + my_dfa.toNFA().accepts("bbbbb"));
             System.out.println("acepta a?:" + my_dfa.toNFA().accepts("a"));
             
-            DFA my_dfa2 = (DFA) FA.parse_form_file("test/test/dfa3.dot");
+            DFA my_dfa2 = (DFA) FA.parse_form_file("test/dfa3.dot");
             System.out.println("vacio dfa3?: "+my_dfa2.is_empty());
 
             /*Set<State> alcanzablesInicial=my_dfa.alcanzablesDesde(my_dfa.initial_state());
